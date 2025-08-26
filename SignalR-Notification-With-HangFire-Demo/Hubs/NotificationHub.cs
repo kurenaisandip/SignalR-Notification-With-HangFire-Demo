0ada000a5bfe395c6ce2dc2333eb870c4999fdce
Sandip <sandip.rai@amniltech.com>
2025-08-26 23:29:38 +0545

    Add NotificationHub for SignalR notifications with authorization

---
+++ SignalR-Notification-With-HangFire-Demo/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR_Notification_With_HangFire_Demo.Hubs;
+
+[Authorize]
+public class NotificationHub : Hub
+{
+    // Optional: server methods if you want to call from clients
+}
+
